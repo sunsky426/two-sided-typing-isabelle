6cc13b23ed3eb45a5cfa182ec96748d12d0d4ce8
Hao <sunskyhao@outlook.com>
2025-09-20 18:49:14 +0200

    added judegments; changed notation to reduce ambiguity

--- MrBNF_ver.thy
+++ MrBNF_ver.thy
@@ -55,7 +55,7 @@ binder_datatype 'var "term" =
   | Pair "'var term" "'var term"
   | Let "(xy::'var) dpair" M::"'var term" N::"'var term" binds xy in N
 
-definition usubst ("_[_ '/ _]" [1000, 49, 49] 1000) where
+definition usubst ("_[_ <- _]" [1000, 49, 49] 1000) where
   "usubst t u x = tvsubst_term (Var(x := u)) t"
 
 lemma SSupp_term_fun_upd: "SSupp_term (Var(x :: 'var :: var := u)) \<subseteq> {x}"
@@ -84,17 +84,17 @@ lemma usubst_simps[simp]:
   unfolding usubst_def using IImsupp_term_fun_upd
   by (subst term.subst; fastforce)+
 
-inductive num :: "'var :: var term \<Rightarrow> bool" where
+inductive num :: "'var::var term \<Rightarrow> bool" where
   "num Zero"
 | "num n \<Longrightarrow> num (Succ n)"
 
-inductive val :: "'var :: var term \<Rightarrow> bool" where
+inductive val :: "'var::var term \<Rightarrow> bool" where
   "val (Var x)"
 | "num n \<Longrightarrow> val n"
 | "val V \<Longrightarrow> val W \<Longrightarrow> val (Pair V W)"
 | "val (Fix f x M)"
 
-inductive beta :: "'var :: var term \<Rightarrow> 'var :: var term \<Rightarrow> bool"  (infix "\<rightarrow>" 70) where
+inductive beta :: "'var::var term \<Rightarrow> 'var::var term \<Rightarrow> bool"  (infix "\<rightarrow>" 70) where
   "N \<rightarrow> N' \<Longrightarrow> App (Fix f x M) N \<rightarrow> App (Fix f x M) N'"
 | "M \<rightarrow> M' \<Longrightarrow> App M N \<rightarrow> App M' N"
 | "M \<rightarrow> M' \<Longrightarrow> Succ M \<rightarrow> Succ M'"
@@ -105,10 +105,10 @@ inductive beta :: "'var :: var term \<Rightarrow> 'var :: var term \<Rightarrow>
 | "M \<rightarrow> M' \<Longrightarrow> If M N P \<rightarrow> If M' N P"
 | Ifz : "If Zero N P \<rightarrow> N"
 | Ifs : "If (Succ n) N P \<rightarrow> P"
-| Let : "Let xy (Pair V W) M \<rightarrow> M[V/dfst xy][W/dsnd xy]"
+| Let : "Let xy (Pair V W) M \<rightarrow> M[V <- dfst xy][W <- dsnd xy]"
 | PredZ: "Pred Zero \<rightarrow> Zero"
 | PredS: "Pred (Succ n) \<rightarrow> n"
-| FixBeta: "App (Fix f x M) V \<rightarrow> M[V/x][Fix f x M/f]"
+| FixBeta: "App (Fix f x M) V \<rightarrow> M[V <- x][Fix f x M <- f]"
 
 binder_inductive (no_auto_equiv) val
   sorry (*TODO: Dmitriy*)
@@ -116,14 +116,14 @@ binder_inductive (no_auto_equiv) val
 binder_inductive (no_auto_equiv) beta
   sorry (*TODO: Dmitriy*)
 
-lemma num_usubst[simp]: "num M \<Longrightarrow> M[V/x] = M"
+lemma num_usubst[simp]: "num M \<Longrightarrow> M[V <- x] = M"
   by (induct M rule: num.induct) auto
 
-lemma val_usubst[simp]: "val M \<Longrightarrow> val V \<Longrightarrow> val (M[V/x])"
+lemma val_usubst[simp]: "val M \<Longrightarrow> val V \<Longrightarrow> val (M[V <- x])"
   by (binder_induction M avoiding: V x rule: val.strong_induct)
     (auto intro: val.intros)
 
-lemma fresh_subst[simp]: "x \<notin> FVars_term t \<Longrightarrow> x \<notin> FVars_term s \<Longrightarrow> x \<notin> FVars_term (t[s / y])"
+lemma fresh_subst[simp]: "x \<notin> FVars_term t \<Longrightarrow> x \<notin> FVars_term s \<Longrightarrow> x \<notin> FVars_term (t[s <- y])"
   sorry (*TODO: Dmitriy*)
 (*
   apply (binder_induction t avoiding: s y rule: term.strong_induct)
@@ -131,7 +131,7 @@ lemma fresh_subst[simp]: "x \<notin> FVars_term t \<Longrightarrow> x \<notin> F
   oops
 *)
 
-lemma subst_idle[simp]: "y \<notin> FVars_term t \<Longrightarrow> t[s / y] = t"
+lemma subst_idle[simp]: "y \<notin> FVars_term t \<Longrightarrow> t[s <- y] = t"
   sorry (*TODO: Dmitriy*)
 (*
   apply (binder_induction t avoiding: s y rule: term.strong_induct)
@@ -139,7 +139,7 @@ lemma subst_idle[simp]: "y \<notin> FVars_term t \<Longrightarrow> t[s / y] = t"
   oops
 *)
 
-lemma usubst_usubst: "y1 \<noteq> y2 \<Longrightarrow> y1 \<notin> FVars_term s2 \<Longrightarrow> t[s1 / y1][s2 / y2] = t[s2 / y2][s1[s2 / y2] / y1]"
+lemma usubst_usubst: "y1 \<noteq> y2 \<Longrightarrow> y1 \<notin> FVars_term s2 \<Longrightarrow> t[s1 <- y1][s2 <- y2] = t[s2 <- y2][s1[s2 <- y2] <- y1]"
   sorry (*TODO: Dmitriy*)
 (*
   apply (binder_induction t avoiding: y1 y2 s1 s2 rule: term.strong_induct)
@@ -150,9 +150,9 @@ lemma usubst_usubst: "y1 \<noteq> y2 \<Longrightarrow> y1 \<notin> FVars_term s2
 lemma dsel_dset[simp]: "dfst xy \<in> dset xy" "dsnd xy \<in> dset xy"
   by (transfer; auto)+
 
-lemma beta_usubst: "M \<rightarrow> N \<Longrightarrow> val V \<Longrightarrow> M[V/x] \<rightarrow> N[V/x]"
+lemma beta_usubst: "M \<rightarrow> N \<Longrightarrow> val V \<Longrightarrow> M[V <- x] \<rightarrow> N[V <- x]"
   apply (binder_induction M N avoiding: M N V x rule: beta.strong_induct)
-               apply (auto intro: beta.intros simp: Int_Un_distrib usubst_usubst[of _ x V])
+  apply (auto intro: beta.intros simp: Int_Un_distrib usubst_usubst[of _ x V])
   apply (subst usubst_usubst[of _ x V])
     apply auto
    apply (metis Int_emptyD dsel_dset(2))
@@ -161,4 +161,38 @@ lemma beta_usubst: "M \<rightarrow> N \<Longrightarrow> val V \<Longrightarrow> 
    apply (metis Int_emptyD dsel_dset(1))
   apply (auto intro: beta.intros)
   done
+
+datatype 'var typing = 
+  Typing "'var term" "type" (infix ":." 70)
+
+inductive disjunction :: "type \<Rightarrow> type \<Rightarrow> bool" (infix "||" 70) where
+  "Nat || Prod _ _"
+| "Nat || To _  _"
+| "Nat || OnlyTo _  _"
+| "Prod _ _ || To _ _"
+| "Prod _ _ || OnlyTo _  _"
+| "A || B \<Longrightarrow> B || A"
+
+inductive judgement :: "'var::var typing set \<Rightarrow> 'var::var typing set \<Rightarrow> bool" (infix "\<turnstile>" 10) where
+  id : "\<Gamma> \<union> {Var x :. A} \<turnstile> {Var x :. A} \<union> \<Delta>"
+| ZeroR : "\<Gamma> \<turnstile> {Zero :. Nat} \<union> \<Delta>"
+| SuccR: "\<Gamma> \<turnstile> {M :. Nat} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {Succ M :. Nat} \<union> \<Delta>"
+| PredR: "\<Gamma> \<turnstile> {M :. Nat} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {Pred M :. Nat} \<union> \<Delta>"
+| FixsR: "\<Gamma> \<union> {Var f :. To A B, Var x :. A} \<turnstile> {M :. B} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {Fix f x M :. To A B} \<union> \<Delta> "
+| FixnR: "\<Gamma> \<union> {Var f :. OnlyTo A B, M :. B} \<turnstile> {Var x :. A} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {Fix f x M :. OnlyTo A B} \<union> \<Delta>"
+| AppR: "\<Gamma> \<union> {M :. To B A} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {N :. B} \<union> \<Delta> \<Longrightarrow>  \<Gamma>  \<turnstile> {App M N :. A} \<union> \<Delta>"
+| PairR: "\<Gamma> \<turnstile> {M :. A} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {N :. B} \<union> \<Delta> \<Longrightarrow>  \<Gamma>  \<turnstile> {Pair M N :. Prod A B} \<union> \<Delta>"
+| LetR: "\<Gamma> \<union> {M :. Prod B C} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Var (dfst x) :. B, Var (dsnd x) :. C} \<turnstile> {N :. A} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {Let x M N :. A} \<union> \<Delta>"
+| IfzR: "\<Gamma> \<turnstile> {M :. Nat} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {P :. A} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {N :. A} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<turnstile> {If M N P :. A} \<union> \<Delta>"
+| Dis: "A || B \<Longrightarrow> \<Gamma> \<turnstile> {M :. B} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<union> {M :. A} \<turnstile> \<Delta>"
+| PairL1: "\<Gamma> \<union> {M :. A} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Pair M N :. Prod A B} \<turnstile> \<Delta>"
+| AppL: "\<Gamma> \<union> {M :. OnlyTo B A} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {N :. B} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {App M N :. A} \<turnstile> \<Delta>"
+| SuccL: "\<Gamma> \<union> {M :. Nat} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Succ M :. Nat} \<turnstile> \<Delta>"
+| PredL: "\<Gamma> \<union> {M :. Nat} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Pred M :. Nat} \<turnstile> \<Delta>"
+| IfzL1: "\<Gamma> \<union> {M :. Nat} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {If M N P :. A} \<turnstile> \<Delta>"
+| IfzL2: "\<Gamma> \<union> {N :. A} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {P :. A} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {If M N P :. A} \<turnstile> \<Delta>"
+| LetL1: "\<Gamma> \<union> {N :. A} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Let x M N :. A} \<turnstile> \<Delta>"
+| LetL2_1: "\<Gamma> \<union> {M :. Prod B1 B2} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {N :. A} \<turnstile> {Var (dfst x) :. B1} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Let x M N :. A} \<turnstile> \<Delta>"
+| LetL2_2: "\<Gamma> \<union> {M :. Prod B1 B2} \<turnstile> \<Delta> \<Longrightarrow> \<Gamma> \<union> {N :. A} \<turnstile> {Var (dsnd x) :. B1} \<union> \<Delta> \<Longrightarrow> \<Gamma> \<union> {Let x M N :. A} \<turnstile> \<Delta>"
+
 end
